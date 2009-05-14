6d105451395f4585cf225f5c310d2af31186c36e
zombor <zombor@ad417eb4-6495-443e-84e6-880146a31082>
2009-05-14 18:28:04 +0000

    Fixing disucssion fetching sql.
    git-svn-id: http://source.kohanaphp.com/svn/simple-forum/trunk@5 ad417eb4-6495-443e-84e6-880146a31082

--- models/forum_discussion.php
+++ models/forum_discussion.php
@@ -25,6 +25,6 @@ class Forum_Discussion_Model extends Auto_Modeler_ORM
 	
 	public function find_discussions($limit = 20, $page = 1)
 	{
-		return $this->db->select('DISTINCT forum_discussions.*')->from('forum_discussions')->join('forum_comments', array('forum_comments.forum_discussion_id' => 'forum_discussions.id'))->limit($limit, ($page-1))->orderby('forum_comments.date')->get()->result(TRUE, 'Forum_Discussion_Model');
+		return $this->db->select('forum_discussions.*')->select(new Database_Expression('MAX(forum_comments.date) AS c_date'))->from('forum_discussions')->join('forum_comments', array('forum_comments.forum_discussion_id' => 'forum_discussions.id'))->limit($limit, ($page-1))->groupby('forum_discussions.id')->orderby('c_date', 'DESC')->get()->result(TRUE, 'Forum_Discussion_Model');
 	}
 }
